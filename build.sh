e9b4ec6de3519f6528818ded0702982894efdc0e
davinahi <kdbj12003@gmail.com>
2025-03-15 12:23:39 +0000

    수정

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd ../
-mkdir output
-cp -R ./scheduler/* ./output
-cp -R ./output ./scheduler/
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import '../style/reset.scss';
-import '../style/style.scss';
-import './App';
-
-const appRoot = document.getElementById('app') as HTMLElement;
-appRoot.innerHTML = `<app-root></app-root>`;
